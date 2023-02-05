df5ee956009901ff776c4dda9aaf6a3da86a2dd1
Olga <olga@Olga-MacBook-Air.local>
2023-02-05 18:22:03 +0200

    Create test - check, change name

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,1 +1,15 @@
-test api
+import pytest
+
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Olga'    
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == 'Maksymets'
