3da02721d024139fc5da37a338b27bb6dc0c55fa
Olga <olga@Olga-MacBook-Air.local>
2023-02-05 18:23:11 +0200

    Create test - check name

---
+++ tests/api/test_fixtures.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.mark.check
+def test_change_name(user):
+    assert user.name == 'Olga'
+
+
+@pytest.mark.check
+def test_change_second_name(user):
+    assert user.second_name == 'Maksymets'
+
+
+    
